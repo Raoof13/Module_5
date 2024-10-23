f1c286d154964e530eced3c2dd297021bd30fb33
Raoof13 <kakmogu23@gmail.com>
2024-10-23 11:33:55 +0300

    20.10

--- module_5_3.py
+++ module_5_3.py
@@ -9,7 +9,8 @@ class House:
         return self.number_of_floors
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
@@ -27,13 +28,16 @@ class House:
         return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value: int):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
 
     def __radd__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
 
     def __iadd__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
