e1341d5ae9332dfebe7fd1252f01071fd00838fa
Raoof13 <kakmogu23@gmail.com>
2024-11-01 22:10:51 +0300

    20.10

--- module5hard.py
+++ module5hard.py
@@ -1,8 +1,11 @@
 # Задание "Свой YouTube"
 
+import time
+
+
 class User:
 
-    def __init__(self, nickname: str, password: str, age):
+    def __init__(self, nickname: str, password: str, age: int):
         self.nickname = nickname
         self.password = hash(password)
         self.age = age
@@ -21,6 +24,13 @@ class Video:
         self.duration = duration
         self.time_now = 0
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f"Video(title={self.title}, duration={self.duration}, adult_mode={self.adult_mode})"
+
 
 class UrTube:
     def __init__(self):
@@ -50,18 +60,38 @@ class UrTube:
             if new_video not in self.videos:
                 self.videos.append(new_video)
 
-    def get_videos(self):
-        ...
+    def get_videos(self, search_word: str):
+        list_of_titles = []
+        for video in self.videos:
+            if search_word.lower() in video.title.lower():
+                list_of_titles.append(video.title)
+        return list_of_titles
 
-    def watch_video(self):
-        ...
-    
-ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-# Добавление видео
-ur.add(v1, v2)
+    def watch_video(self, name_film: str):
+        if not self.current_user:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+
+        if _film.adult_mode and self.current_user.age < 18:
+            print("Вам нет 18 лет, пожалуйста покиньте страницу")
+            return
+
+        for i in range(_film.duration):
+                    print(_film, end=' ')
+                    time.sleep(1)
+                    _film.time_now += 1
+                _film.time_now = 0
+                print('Конец видео')
+
+if __name__ == '__main__':
+    ur = UrTube()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+    # Добавление видео
+    ur.add(v1, v2)
+
 
 # Проверка поиска
 print(ur.get_videos('лучший'))
